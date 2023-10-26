572f70cfada81a4b32e15ddec326540d5a6e4979
Avi Tuchband <avitoohband@gmail.com>
2023-10-26 18:18:41 +0300

    finished login/register

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,8 +28,8 @@ export const register = async (req, res) => {
       friends,
       location,
       occupation,
-      viewedProfile: Math.floor(Math.random() * 1000),
-      impressions: Math.floor(Math.random() * 1000),
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
@@ -38,26 +38,19 @@ export const register = async (req, res) => {
   }
 };
 
-/* LOGING IN */
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-
-    if (!user) {
-      return res.status(400).json({ msg: "User does not exists." });
-    }
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
     const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid credentials." });
-    }
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    const userObj = user.toObject();
-    delete userObj.password;
-    res.status(200).json({ token, userObj });
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,16 @@ export const getUserFriends = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
-    return getFriendsHelper(user);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
@@ -26,25 +35,18 @@ export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
     const user = await User.findById(id);
-    const friend = await user.findById(friendId);
+    const friend = await User.findById(friendId);
 
-    if (user.friends.includes(friend)) {
+    if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((id) => id !== id);
     } else {
-      user.friends.push(friend);
+      user.friends.push(friendId);
       friend.friends.push(id);
     }
     await user.save();
     await friend.save();
-    return getFriendsHelper(user);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
 
-const getFriendsHelper = async (user) => {
-  try {
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     );
@@ -53,7 +55,8 @@ const getFriendsHelper = async (user) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
-    return res.status(200).json(formattedFriends);
+
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
